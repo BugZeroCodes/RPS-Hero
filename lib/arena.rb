1d8e21dff5102e63483dd6f6235f81fa1c88a051
BugZeroCodes <timothy@timothys-air.attlocal.net>
2018-02-04 16:09:38 -0500

    Finish up the RPS-hero

--- lib/arena.rb
+++ lib/arena.rb
@@ -3,4 +3,22 @@ class Arena
     @hero1 = hero1
     @hero2 = hero2
   end
+
+  def apply_damage(doing_damage)
+    if doing_damage == :hero1
+      @hero2.receive_damage(@hero1.power)
+    elsif doing_damage == :hero2
+      @hero1.receive_damage(@hero2.power)
+    end
+  end
+
+  def winner
+    if @hero1.health == 0
+      return @hero2.name
+    elsif @hero2.health == 0
+      return @hero1.name
+    else
+      return nil
+    end
+  end
 end
