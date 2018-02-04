bbd5a283e9152fb818c05994b750c533e9196fe6
BugZeroCodes <timothy@timothys-air.attlocal.net>
2018-02-04 15:33:36 -0500

    Use let to clean code

--- spec/arena_spec.rb
+++ spec/arena_spec.rb
@@ -1,9 +1,11 @@
 require_relative '../lib/arena'
 RSpec.describe 'Arena' do
+
+  let(:hero1) { Hero.new('Jeff', 140, 150, 45, 56) }
+  let(:hero2) { Hero.new('Blake', 190, 200, 45, 56) }
+  let(:mak) { Hero.new('The Masked Man', 10000, 10000, 100, 900) }
+
   it 'initializes 2 heroes' do
-    hero1 = Hero.new('Jeff', 140, 150, 45, 56)
-    hero2 = Hero.new('Blake', 190, 200, 45, 56)
-    mak = Hero.new('The Masked Man', 10000, 10000, 100, 900)
     expect(Arena.new(hero1, hero2)).to be_an_instance_of(Arena)
   end
 end
