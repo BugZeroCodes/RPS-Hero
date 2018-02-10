f96250087e224e4f6290744a3d9208183d80c757
cfaloon <cfaloon@gmail.com>
2018-02-10 11:36:48 -0500

    Reduce number of heroes

--- spec/hero_spec.rb
+++ spec/hero_spec.rb
@@ -2,35 +2,43 @@ require_relative '../lib/hero.rb'
 
 RSpec.describe 'Hero' do
 
-  let(:reginald) { Hero.new(name: 'Reginald', health: 100, max_health: 100, power: 45, armor: 30) }
-  let(:tom) { Hero.new(name: "Tom", health: 345, max_health: 345, power: 45) }
-  let(:jeff) { Hero.new(name: 'Jeff', health: 100, max_health: 100, power: 6, armor: 30) }
-  let(:blake) { Hero.new(name: 'Blake', health: 190, max_health: 100, power: 45, armor: 56) }
+  let(:jeff) { Hero.new(name: 'Jeff', health: 100, max_health: 100, power: 6) }
 
-  it 'has health' do
-    expect(reginald.health).to eq(100)
+  it 'has a name' do
+    expect(jeff.name).to eq('Jeff')
   end
-  it 'has max health' do
-    expect(tom.max_health).to eq(345)
+  
+  context 'health' do
+    it 'has health' do
+      expect(jeff.health).to eq(100)
+    end
+    
+    it 'has max health' do
+      expect(jeff.max_health).to eq(100)
+    end
   end
+  
   it 'has power' do
     expect(jeff.power).to eq(6)
   end
-  it 'has armor/power resistance' do
-    expect(blake.armor).to eq(56)
+
+  context 'armor' do
+    it 'has armor/power resistance' do
+      expect(jeff.respond_to?(:armor)).to be true
+    end
+    it "expects default value of hero's armor to be 5" do
+      expect(jeff.armor).to eq(5)
+    end
   end
-  it 'has a name' do
-    expect(blake.name).to eq('Blake')
-  end
-  it "recieves damage" do
-    jeff.receive_damage(20)
-    expect(jeff.health).to eq(80)
-  end
-  it "receives fatal damage" do
-    jeff.receive_damage(170)
-    expect(jeff.health).to eq(0)
-  end
-  it "expects default value of hero's armor to be 5" do
-    expect(tom.armor).to eq(5)
+  
+  context 'damage' do
+    it "recieves damage" do
+      jeff.receive_damage(20)
+      expect(jeff.health).to eq(80)
+    end
+    it "receives fatal damage" do
+      jeff.receive_damage(170)
+      expect(jeff.health).to eq(0)
+    end
   end
 end
