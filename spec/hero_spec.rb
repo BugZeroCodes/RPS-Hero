71c6e5a2f48885d5dbb91e7e95db3571ec81349d
cfaloon <cfaloon@gmail.com>
2018-02-10 12:55:18 -0500

    Add Character spec and expect Hero to be a subclass of Character

--- spec/hero_spec.rb
+++ spec/hero_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe 'Hero' do
 
   let(:jeff) { Hero.new(name: 'Jeff', health: 100, max_health: 100, power: 6) }
 
+  it 'is a kind of Character' do
+    expect(jeff).to be_kind_of(Character)
+  end
+  
   it 'has a name' do
     expect(jeff.name).to eq('Jeff')
   end
